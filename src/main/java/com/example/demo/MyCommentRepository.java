a963fd5118b04ace59d945be6259ba0cb6960523
hwangbh-mike <55347103+hwangbh-mike@users.noreply.github.com>
2019-09-21 17:17:42 +0900

    jpa query possible test sync/async

--- src/main/java/com/example/demo/MyCommentRepository.java
+++ src/main/java/com/example/demo/MyCommentRepository.java
@@ -5,18 +5,27 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.util.concurrent.ListenableFuture;
 
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
+@EnableAsync
 public interface MyCommentRepository extends MyReposInterface<MyComment,Long> {
 
   //  @Query(value = "select c from MyComment c", nativeQuery=true)
     List<MyComment> findByTitleContainsIgnoreCase(String keyword);
 
-    Page<MyComment> findByBigTestGreaterThan(int bigTest, Pageable pageable);
+    Page<MyComment> findByBigTestGreaterThanOrderByTitleDesc(int bigTest, Pageable pageable);
 
+    Stream<MyComment> findByBigTestGreaterThanOrderByTitleAsc(int bigTest, Pageable pageable);
 
+    @Async
+    ListenableFuture<List<MyComment>> findByBigTestLessThanOrderByTitleAsc(int bigTest, Pageable pageable);
 
 
 
--- src/test/java/com/example/demo/MyCommentRepositoryTest.java
+++ src/test/java/com/example/demo/MyCommentRepositoryTest.java
@@ -8,11 +8,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.lang.Nullable;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -27,19 +34,55 @@ public class MyCommentRepositoryTest {
     MyCommentRepository myCommentRepository;
 
     @Test
-    public void crudTest22(){
+    public void crudTest22() throws ExecutionException, InterruptedException {
 
         MyComment myComment = new MyComment();
-        myComment.setTitle("hello test name");
+        myComment.setTitle("ccccchello test name");
         myComment.setBigTest(new Integer(100));
         myCommentRepository.save(myComment);
 
+        MyComment myComment1 = new MyComment();
+        myComment1.setTitle("aaahello test name");
+        myComment1.setBigTest(new Integer(101));
+        myCommentRepository.save(myComment1);
+
+        MyComment myComment2 = new MyComment();
+        myComment2.setTitle("bbbbhellobbbb test name");
+        myComment2.setBigTest(new Integer(1021));
+        myCommentRepository.save(myComment2);
+
 
         List<MyComment> all = myCommentRepository.findByTitleContainsIgnoreCase("HELLO");
-        assertThat(all.size()).isEqualTo(1);
+        assertThat(all.size()).isEqualTo(3);
 
-        Page<MyComment> page = myCommentRepository.findByBigTestGreaterThan(1000, PageRequest.of(0,10));
-        assertThat(page.getNumberOfElements()).isEqualTo(1);
+        Page<MyComment> page = myCommentRepository.findByBigTestGreaterThanOrderByTitleDesc(10, PageRequest.of(0,10));
+        page.getContent().forEach(s-> {
+            System.out.println("-----------------------");
+            System.out.println(s.getTitle());
+        });
+        assertThat(page.getNumberOfElements()).isEqualTo(3);
+
+        Stream<MyComment> p = myCommentRepository.findByBigTestGreaterThanOrderByTitleAsc(10, PageRequest.of(0,10));
+        assertThat(p.findFirst().get().getBigTest()).isEqualTo(101);
+
+        ListenableFuture<List<MyComment>> f =  myCommentRepository.findByBigTestLessThanOrderByTitleAsc(105, PageRequest.of(0,10));
+//        f.get().forEach(s->{
+//            log.warn("&&&&&&&&&&&&&&&&&&&&&&&&&");
+//            log.warn(s.getTitle());
+//        });
+
+        f.addCallback(new ListenableFutureCallback<List<MyComment>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.warn("failure...................");
+            }
+
+            @Override
+            public void onSuccess(@Nullable List<MyComment> myComments) {
+                log.warn("success....................");
+            }
+        });
+
 
 //        long count = myCommentRepository.count();
 //        assertThat(count).isEqualTo(1);
